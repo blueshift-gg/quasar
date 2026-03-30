12129bc8e75acd34dbf078e09e9bfbd395cd5482
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2026-03-30 19:43:48 -0300

    fmt

--- examples/upstream-vault/src/tests.rs
+++ examples/upstream-vault/src/tests.rs
@@ -6,7 +6,8 @@ use {
 };
 
 fn setup() -> QuasarSvm {
-    let elf = std::fs::read("../../target/bpfel-unknown-none/release/libupstream_vault.so").unwrap();
+    let elf =
+        std::fs::read("../../target/bpfel-unknown-none/release/libupstream_vault.so").unwrap();
     QuasarSvm::new().with_program(&crate::ID, &elf)
 }
 
